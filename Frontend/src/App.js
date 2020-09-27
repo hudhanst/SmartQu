83f25da6761e45fd3178d8c12b083209aebedaea
hudhanst <60328630+hudhanst@users.noreply.github.com>
2020-09-27 21:36:47 +0700

    SmartQu - initialitation ***Backend*** -init -npm i express mongoose @tensorflow/tfjs-node node-schedule nodemon (D) ***Frontend*** -npx create-react-app -npm i @material-ui/core axios react-icons react-redux react-router-dom redux redux-thunk -add logo & inspiration text

---
+++ Frontend/src/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+
+const App = () => {
+  return (
+    <Fragment>
+      <center>
+        <h1>tes</h1>
+      </center>
+    </Fragment>
+  )
+}
+
+export default App
